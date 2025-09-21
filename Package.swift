5f2899c1ef877813e56a4ee9351ce159e3bb0854
Maxnxi <nisq@ya.ru>
2025-09-21 11:45:29 +0700

    correct ios version

--- Package.swift
+++ Package.swift
@@ -2,29 +2,6 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-//
-//let package = Package(
-//	name: "ModernSwiftStorage",
-//	platforms: [
-//		.iOS(.v13),
-//		.macOS(.v10_15),
-//		.watchOS(.v6),
-//		.tvOS(.v13)
-//	],
-//	products: [
-//		.library(
-//			name: "ModernSwiftStorage",
-//			targets: ["ModernSwiftStorage"]
-//		),
-//	],
-//	targets: [
-//		.target(
-//			name: "ModernSwiftStorage",
-//			dependencies: []
-//		)
-//	]
-//)
-
 
 let package = Package(
 	name: "ModernSwiftStorage",
